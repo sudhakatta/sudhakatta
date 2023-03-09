abd6b591da363a52ec0f8bb1aca8cdcb74429019
sudha <sudha4katta@gmail.com>
2023-03-09 20:27:18 +0530

    01 commit

--- learningshell/01-shebang.sh
+++ learningshell/01-shebang.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #my name is sudha
+
+echo "--------------- Here is hostname---------"
 hostname
+echo "--------------- Here is list of files---------"
 ls -ltr
+echo "--------------- Here is date---------"
 date
