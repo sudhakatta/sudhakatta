9521983bdb4c9561ee0c07f19dcc582eb97ef709
sudha <sudha4katta@gmail.com>
2023-03-09 22:52:07 +0530

    if

--- learningshell/06-if.sh
+++ learningshell/06-if.sh
@@ -2,10 +2,10 @@
 a=11
 if [ $a -gt 10 ] 
 then 
-echo "$a gt than 10"
+echo $a gt than 10
 else if [ $a -lt 10]
 
-echo "$a lt than 10"
+echo $a lt than 10
 else
-echo "$a ls than 10"
+echo $a ls than 10
 fi
