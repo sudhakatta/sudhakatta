a819c91a520ca7a8b717c5150201579edd32785b
sudha <sudha4katta@gmail.com>
2023-03-11 20:27:06 +0530

    n

--- roboshopshellscripting/common.sh
+++ roboshopshellscripting/common.sh
@@ -138,4 +138,27 @@ echo "Installing dependencies"
  go get &>>${LOG_FILE}
  go build &>>${LOG_FILE}
 SYSTEMD_SETUP
+
+
+
+
+echo " Installing golang "
+    yum install golang -y &>>${LOG_FILE}
+    Statuscheck $?
+
+    APP_PREREQ
+
+    echo " Installing the dependencies "
+    cd /home/roboshop
+    cd ${COMPONENT}
+    #cd /home/${COMPONENT}/
+    go mod init dispatch &>>${LOG_FILE}
+    Statuscheck $?
+
+    echo "Doing the golang build"
+    go get 
+    go build &>>${LOG_FILE}
+    Statuscheck $?
+
+    SYSTEMD_SETUP
 }
