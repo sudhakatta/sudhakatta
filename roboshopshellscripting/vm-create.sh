a81f299e9a5d6e84076a11918465c21d317d4a50
sudha <sudha4katta@gmail.com>
2023-03-13 21:44:18 +0530

    u

---
+++ roboshopshellscripting/vm-create.sh
@@ -0,0 +1,8 @@
+echo " Creating Vm automatically using CLI "
+
+
+
+for component in catalogue user cart payment shipping frontend mongodb redis mysql rabbitmq dispatch ; do
+az vm create --resource-group SUDHA_RG --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name SUDHA_RG-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s
+az vm auto-shutdown -g SUDHA_RG -n  $component --time 1230  
+done
